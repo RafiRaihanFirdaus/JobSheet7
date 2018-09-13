2a892bc546a7836ae06db99d0f8ac8a7620582e2
Owner <Owner@DESKTOP-3762CMA>
2018-09-13 13:04:54 +0700

    Initial Commit

---
+++ src/dynamicprogram/HitungFibonacci.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dynamicprogram;
+import java.util.Scanner;
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas: " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+        
+        String Identitas  = "Rafi Raihan F / X RPL 2 / 26";
+        tampilJudul(identitas);
+    }
+    
+}
