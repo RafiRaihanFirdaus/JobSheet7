d3fdfafc470f078d72f7fede971d850ea3d355ff
Owner <Owner@DESKTOP-3762CMA>
2018-09-13 13:16:17 +0700

    Tambah Hitung Fibonacci

--- src/dynamicprogram/HitungFibonacci.java
+++ src/dynamicprogram/HitungFibonacci.java
@@ -26,6 +26,20 @@ public class HitungFibonacci {
     }
     int n = tampilInput();
     
-    
+     private static BigInteger fibo(int n) {
+        
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++) {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        
+        return hasil[n-1];
+    }
+     
+     BigInteger hasil = fibo(n);
     
 }
